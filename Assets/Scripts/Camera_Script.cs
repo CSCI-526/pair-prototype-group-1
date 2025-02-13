ba4ae758de91f1826ff1b275fbd3c791d7bb231b
Jimmy Taravia <taraviajimmy01@gmail.com>
2025-02-13 02:40:01 -0800

    obstacles updated

--- Assets/Scripts/Camera_Script.cs
+++ Assets/Scripts/Camera_Script.cs
@@ -13,6 +13,6 @@ public class Camera_Script : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z - 11.19f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z - 5f);
     }
 }
